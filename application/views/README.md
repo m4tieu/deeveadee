31a368cc604d71defa0d7110b7f3ef608d1955a0
m4tieu <mathieu.becerra.beweb@gmail.com>
2017-06-09 17:01:09 +0200

    THE commit

---
+++ application/views/README.md
@@ -0,0 +1,21 @@
+# deeveadee
+
+**Evaluations dvdthèque**
+
+##### Technologie utiliser pour cette évaluation:
+
+* [Codeigniter  3.1.4] - Framwork
+* [Bootstrap 3.3.7] - Framwork
+* [MySQL Workbench 6.3.9] - Diagrame
+* [phpMyAdmin 4.7.1] - Interface gestion de db
+* [jQuery 3.2.1] - jQuery pour AJAX
+* [Dillinger] - Compilateur readme
+
+
+   [Codeigniter 3.1.4]: <https://codeigniter.com/download>
+   [Bootstrap 3.3.7]: <http://getbootstrap.com/getting-started/#download>
+   [jQuery 3.2.1]: <https://jquery.com/download/>
+   [MySQL Workbench 6.3.9]: <https://dev.mysql.com/downloads/workbench/>
+   [phpMyAdmin 4.7.1]: <https://www.phpmyadmin.net/>
+   [Dillinger]: <http://dillinger.io/>
+
---
+++ application/views/index.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+        <title>DeeVeaDee</title>
+        <meta charset="UTF-8">
+        <link rel="shortcut icon" href="../script/PICTURE/dvd.ico"/>
+        <link rel="stylesheet" href="../script/CSS/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="../script/CSS/style.css"/>
+    </head>
+    <body class="texte-center">
+        <header>
+            <h1 class="text-center"><a href="index">DeeVeaDee</a></h1>
+        </header>
+        <nav class="navbar navbar-inverse" id="nav">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">DeeVeaDee</a>
+                </div>
+                <ul class="nav navbar-nav navbar-left">
+                    <li><a href="#">Accueil</a></li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Genre <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Animation</a></li>
+                            <li><a href="#">Aventure</a></li>
+                            <li><a href="#">Biographie</a></li>
+                            <li><a href="#">Bollywood</a></li>
+                            <li><a href="#">Catastrophe</a></li>
+                            <li><a href="#">Comédie</a></li>
+                            <li><a href="#">Documentaire</a></li>
+                            <li><a href="#">Dramatique</a></li>
+                            <li><a href="#">Espionnage</a></li>
+                            <li><a href="#">Fantastique</a></li>
+                            <li><a href="#">Survie</a></li>
+                            <li><a href="#">Guerre</a></li>
+                            <li><a href="#">Historique</a></li>
+                            <li><a href="#">Horreur</a></li>
+                            <li><a href="#">Musical</a></li>
+                            <li><a href="#">Policier</a></li>
+                            <li><a href="#">Romantique</a></li>
+                            <li><a href="#">Science-Fiction</a></li>
+                            <li><a href="#">Sketches</a></li>
+                            <li><a href="#">Thriller</a></li>
+                            <li><a href="#">Western</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Année <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Nouveautés</a></li>
+                            <li><a href="#">Années 2010</a></li>
+                            <li><a href="#">Années 2000</a></li>
+                            <li><a href="#">Années 1990</a></li>
+                            <li><a href="#">Années 1980</a></li>
+                            <li><a href="#">Années 1970</a></li>
+                            <li><a href="#">Années 1960</a></li>
+                            <li><a href="#">Années 1950 et +</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><img class="adult" src="/script/PICTURE/18xl.png"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Films Érotique</a></li>
+                            <li><a href="#">Films Pornographique</a></li>
+                        </ul>
+                    </li>
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Titre, Genre, Acteur...">
+                        </div>
+                        <button type="submit" class="btn btn-default">Go
+                        </button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><img src="/script/PICTURE/user.png"> Mon compte</a></li>
+                        <li><a href="#"><img src="/script/PICTURE/logout.png"> Deconnexion</a></li>
+                    </ul>
+                </ul>
+            </div>
+        </nav>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bdd">
+            <h2 class="text-center">{{APPELS BDD}}</h2>
+        </div>
+        <footer class="text-center">
+            <p> © 2016 - 2017 by <a href="http://hsource.fr/trollpages/1">m4tieu</a>.</p>
+        </footer>
+        <script src="../script/JS/jquery.js"></script>
+        <script src="../script/JS/bootstrap.js"></script>
+    </body>
+</html>'
